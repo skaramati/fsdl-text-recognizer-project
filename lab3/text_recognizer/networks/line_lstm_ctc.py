5a26dc7ff8bd97be0547a30a736c94ed56616e6c
skaramati <skaramati@local>
2018-08-04 00:53:23 +0000

    my lab3 work

--- lab3/text_recognizer/networks/line_lstm_ctc.py
+++ lab3/text_recognizer/networks/line_lstm_ctc.py
@@ -50,7 +50,7 @@ def line_lstm_ctc(input_shape, output_shape, window_width=28, window_stride=14):
     convnet_outputs = TimeDistributed(convnet)(image_patches)
     # (num_windows, 128)
 
-    lstm_output = lstm_fn(128, return_sequences=True)(convnet_outputs)
+    lstm_output = lstm_fn(256, return_sequences=True)(convnet_outputs)
     # (num_windows, 128)
 
     softmax_output = Dense(num_classes, activation='softmax', name='softmax_output')(lstm_output)
